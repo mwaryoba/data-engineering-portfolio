e9f6121df5cd14249f3394987d520bcfe9270960
mwaryoba <michaelwaryoba@gmail.com>
2025-04-26 22:49:22 -0400

    Added pendo api ingestion notebooks & utilities. (#6)

---
+++ pendo_api_ingestion/utilities/PendoApiUtil.py
@@ -0,0 +1,65 @@
+# Databricks notebook source
+# MAGIC %run ../Utilities/PendoApiUtil
+
+# COMMAND ----------
+
+# MAGIC %run ../../../Utilities/Elt/EltUtil
+
+# COMMAND ----------
+
+try:
+    environment = dbutils.widgets.get("env") 
+except:
+    environment = 'dev'
+
+# COMMAND ----------
+
+ 
+az = AzureKv(environment)
+snow = SnowflakeUtil(env = environment, 
+                     user = az.get_secret('azure-snowflake-user'), 
+                     password = az.get_secret('azure-snowflake-password'), 
+                     database ='DSV_ELT_INGESTION' , 
+                     schema = 'PENDO', 
+                     warehouse = 'DSV_ELT_CP')
+
+# COMMAND ----------
+
+try:
+    pendo_subscription_type = dbutils.widgets.get("pendo_subscription")
+    integ_key = az.get_secret('pendo-customer-portal-api-integration-key') if pendo_subscription_type else None
+except:
+    pendo_subscription_type = None
+    integ_key = None
+
+# COMMAND ----------
+
+print(pendo_subscription_type)
+
+# COMMAND ----------
+
+pendo = PendoApiUtil('accounts', integ_key=integ_key)
+
+# COMMAND ----------
+
+extract_date = to_utc_timestamp(current_timestamp(),"UTC")
+res = pendo.send_request()
+
+# COMMAND ----------
+
+sdf = DatabricksUtil.df_jsonize_array_w_extract_date(res.json().get('results'), extract_date)
+
+# COMMAND ----------
+
+sdf = sdf.withColumn('load_date', to_utc_timestamp(current_timestamp(),"UTC"))
+
+# COMMAND ----------
+
+if pendo_subscription_type:
+    snow.write_df(sdf, 'pendo.raw_pendo_customer_portal_account', 'append')
+else:
+    snow.write_df(sdf, 'pendo.raw_pendo_account', 'append')
+
+# COMMAND ----------
+
+sdf.unpersist()
